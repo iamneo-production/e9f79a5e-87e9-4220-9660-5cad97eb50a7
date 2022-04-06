37f0ef982030c75a5deb3d872b7958d927118ee4
SindhuIrrinki <100984433+SindhuIrrinki@users.noreply.github.com>
2022-04-07 05:04:16 +0530

    Add files via upload

---
+++ signup.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder} from '@angular/forms'
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  public signupForm !: FormGroup;
+  constructor(private formBuilder : FormBuilder) { }
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      admin: [''],
+      email: [''],
+      username: [''],
+      mobileNumber: [''],
+      password: [''],
+      confirmPassword: ['']
+    })
+  }
+  signUp(){
+
+  }
+}
